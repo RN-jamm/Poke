10e6427f59f8ba78326dbcb68297eba0aa9666b9
RN <nlider7211@gmail.com>
2023-02-01 19:50:46 +0100

    clean ImageHelper, reflection Tiles

--- src/background/TileManager.java
+++ src/background/TileManager.java
@@ -7,22 +7,25 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
-
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TileManager {
 
     GamePanel gp;
-    Tile[] tile;
-    int[][] mapTileNum;
+    ArrayList<Tile> tile;
+    ArrayList<ArrayList<HashMap<String, String>>> mapTileNum;
 
     BufferedImage tileSheet;
 
     public TileManager(GamePanel gp, String imagePath) {
         this.gp = gp;
         tileSheet = ImageHelper.getImageSheet(imagePath);
-        tile = new Tile[getTileSize()];
+        tile = new ArrayList<>();
         setTileImages();
-        mapTileNum = new int[gp.maxMapRow][gp.maxMapCol];
+        mapTileNum = new ArrayList<>();
+
+//        mapTileNum = new HashMap[gp.maxMapRow][gp.maxMapCol];
 //        loadMap("res/maps/map02.txt");
         try {
             imgToMap("res/sprites/background/cavesofgallet.png");
@@ -32,15 +35,6 @@ public class TileManager {
 
     }
 
-    private int getTileSize() {
-        int size = 0;
-        for (int x = 0; x < tileSheet.getWidth()/8; x++) {
-            for (int y = 0; y < tileSheet.getHeight()/8; y++) {
-                size++;
-            }
-        }
-        return size;
-    }
 
     public void setTileImages() {
         int col = 0;
@@ -49,8 +43,8 @@ public class TileManager {
         int i = 0;
         while (col < tileSheet.getWidth()/8 && row < tileSheet.getHeight()/8) {
             while (col < tileSheet.getWidth()/8) {
-                tile[i] = new Tile();
-                tile[i].image = tileSheet.getSubimage(col*8, row*8, 8, 8);
+                tile.add(new Tile());
+                tile.get(i).image = tileSheet.getSubimage(col*8, row*8, 8, 8);
                 col++;
                 i++;
             }
@@ -61,10 +55,30 @@ public class TileManager {
         }
     }
 
-    public void loadMap(String mapPath) {
+    public void setTileCollisions() {
+
+        int col = 0;
+        int row = 0;
+
+        int i = 0;
+        while (col < tileSheet.getWidth()/8 && row < tileSheet.getHeight()/8) {
+            while (col < tileSheet.getWidth()/8) {
+                tile.get(i).image = tileSheet.getSubimage(col*8, row*8, 8, 8);
+                col++;
+                i++;
+            }
+            if (col == tileSheet.getWidth()/8) {
+                col = 0;
+                row++;
+            }
+        }
+    }
+
+    public void loadMapFromTxt(String mapPath) {
         try {
             File file = new File(mapPath);
             BufferedReader br = new BufferedReader(new FileReader(file));
+            ArrayList<HashMap<String, String>> tmpList;
 
             int col = 0;
             int row = 0;
@@ -73,12 +87,18 @@ public class TileManager {
 
                 String line = br.readLine();
 
+                tmpList = new ArrayList<>();
                 while (col < gp.maxMapCol) {
                     String[] numbers = line.split(" ");
                     int num = Integer.parseInt(numbers[col]);
-                    mapTileNum[col][row] = num;
+                    HashMap<String, String> setOfInfo= new HashMap<>();
+                    setOfInfo.put("tileNum", String.format("%d", num));
+//                    setOfInfo.put("reflectionType", String.format("%d", reflectionType));
+//                    setOfInfo.put("collision", String.format("%d", reflectionType));
+                    tmpList.add(setOfInfo);
                     col++;
                 }
+                mapTileNum.add(tmpList);
                 if (col == gp.maxMapCol) {
                     col = 0;
                     row++;
@@ -92,28 +112,43 @@ public class TileManager {
 
     public void imgToMap(String mapImgPath) {
         try {
+            ArrayList<HashMap<String, String>> tmpList;
             BufferedImage mapImg, imgBlock;
             mapImg = ImageIO.read(new File(mapImgPath));
+            int reflectionType;
 
             int col = 0;
             int row = 0;
 
             while (col < gp.maxMapCol && row < gp.maxMapRow) {
+                tmpList = new ArrayList<>();
                 while (col < gp.maxMapCol) {
                     imgBlock = mapImg.getSubimage(col*8, row*8, 8, 8);
-                    for (int i=0; i< tile.length; i++) {
-                        if (ImageHelper.compareTwoImages(tile[i].image, imgBlock)) {
-                            mapTileNum[row][col] = i;
+                    for (int i=0; i< tile.size(); i++) {
+
+                        // -1 means False in this function
+                        // other numbers == type of reflection (check ImageHelper)
+                        reflectionType = ImageHelper.compareImgToReflections(tile.get(i).image, imgBlock);
+
+                        if (reflectionType != -1) {
+                            HashMap<String, String> setOfInfo= new HashMap<>();
+                            setOfInfo.put("tileNum", String.format("%d", i));
+                            setOfInfo.put("reflectionType", String.format("%d", reflectionType));
+//                            setOfInfo.put("collision", String.format("%d", reflectionType));
+                            tmpList.add(setOfInfo);
                             break;
                         }
                     }
                     col++;
                 }
+                mapTileNum.add(tmpList);
                 if (col == gp.maxMapCol) {
                     col = 0;
                     row++;
                 }
             }
+
+            // code which makes map in txt format... for now not needed
 //            try {
 //                BufferedWriter writer = new BufferedWriter(new FileWriter("res/maps/mapTest"));
 //                String line = "";
@@ -146,7 +181,7 @@ public class TileManager {
         // here aswell we need bigger map of 1
         int Col = -1;
         int Row = -1;
-        int currentMapNum;
+        int currentMapNum, reflectionType;
 
 
         // render map (outer map is basicTile = tile[0]) |
@@ -161,10 +196,14 @@ public class TileManager {
 
             if ((mapCol < 0 || mapCol >= gp.maxMapCol) || (mapRow < 0 || mapRow >= gp.maxMapRow)) {
                 currentMapNum = 0;
+                reflectionType = 0;
             } else {
-                currentMapNum = mapTileNum[mapRow][mapCol];
+                currentMapNum = Integer.parseInt(mapTileNum.get(mapRow).get(mapCol).get("tileNum"));
+                reflectionType = Integer.parseInt(mapTileNum.get(mapRow).get(mapCol).get("reflectionType"));
             }
-            g2.drawImage(tile[currentMapNum].image, screenX, screenY, gp.TileSize, gp.TileSize, null);
+            // reflectByType reflects image to the form that it appears on ImgMap
+            // some tiles on ImgMap are reflected
+            g2.drawImage(ImageHelper.reflectByType(tile.get(currentMapNum).image, reflectionType), screenX, screenY, gp.TileSize, gp.TileSize, null);
             mapCol++;
             Col++;
 
@@ -176,120 +215,5 @@ public class TileManager {
                 Row++;
             }
         }
-
-
-        // This whole code is pointless for now but I will leave it just in case I need it in future
-        // when I will try to add more maps
-
-//        // Stoping render of left wall
-//        else if ((mapCol <= 0) && (mapRow > 0 && mapRow < gp.maxMapRow)) {
-//            mapCol = 0;
-//            while (Col <= gp.maxScreenCol + 1 && Row <= gp.maxScreenRow + 1) {
-//
-//                int worldX = mapCol * gp.TileSize;
-//                int worldY = mapRow * gp.TileSize;
-//                int screenX = worldX - gp.player.worldX + gp.player.screenX;
-//                int screenY = worldY - gp.player.worldY + gp.player.screenY;
-//
-//                if ((mapCol < 0 || mapCol > gp.maxMapCol) || (mapRow < 0 || mapRow > gp.maxMapRow)) {
-//                    currentMapNum = 0;
-//                } else {
-//                    currentMapNum = mapTileNum[mapRow][mapCol];
-//                }
-//                g2.drawImage(tile[currentMapNum].image, screenX, screenY, gp.TileSize, gp.TileSize, null);
-//                mapCol++;
-//                Col++;
-//
-//                if (Col == gp.maxScreenCol + 1) {
-//                    mapCol = 0;
-//                    Col = -1;
-//                    mapRow++;
-//                    Row++;
-//                }
-//            }
-//        }
-//
-//        // Stoping render of right wall
-//        else if ((mapCol > gp.maxMapCol) && (mapRow > 0 && mapRow < gp.maxMapRow)) {
-//            mapCol = gp.maxMapCol;
-//            while (Col <= gp.maxScreenCol + 1 && Row <= gp.maxScreenRow + 1) {
-//
-//                int worldX = mapCol * gp.TileSize;
-//                int worldY = mapRow * gp.TileSize;
-//                int screenX = worldX - gp.player.worldX + gp.player.screenX;
-//                int screenY = worldY - gp.player.worldY + gp.player.screenY;
-//
-//                if ((mapCol < 0 || mapCol > gp.maxMapCol) || (mapRow < 0 || mapRow > gp.maxMapRow)) {
-//                    currentMapNum = 0;
-//                } else {
-//                    currentMapNum = mapTileNum[mapRow][mapCol];
-//                }
-//                g2.drawImage(tile[currentMapNum].image, screenX, screenY, gp.TileSize, gp.TileSize, null);
-//                mapCol++;
-//                Col++;
-//
-//                if (Col == gp.maxScreenCol - 1) {
-//                    mapCol = gp.maxMapCol;
-//                    Col = -1;
-//                    mapRow--;
-//                    Row++;
-//                }
-//            }
-//        }
-//        // Stoping render of up wall
-//        else if ((mapCol > 0 && mapCol < gp.maxMapCol-1) && (mapRow <= 0)) {
-//            mapRow = 0;
-//            while (Col <= gp.maxScreenCol + 1 && Row <= gp.maxScreenRow + 1) {
-//
-//                int worldX = mapCol * gp.TileSize;
-//                int worldY = mapRow * gp.TileSize;
-//                int screenX = worldX - gp.player.worldX + gp.player.screenX;
-//                int screenY = worldY - gp.player.worldY + gp.player.screenY;
-//
-//                if ((mapCol < 0 || mapCol > gp.maxMapCol) || (mapRow < 0 || mapRow > gp.maxMapRow)) {
-//                    currentMapNum = 0;
-//                } else {
-//                    currentMapNum = mapTileNum[mapRow][mapCol];
-//                }
-//                g2.drawImage(tile[currentMapNum].image, screenX, screenY, gp.TileSize, gp.TileSize, null);
-//                mapCol++;
-//                Col++;
-//
-//                if (Col == gp.maxScreenCol + 1) {
-//                    mapCol = (gp.player.worldX - gp.screenWidth / 2) / gp.TileSize;
-//                    Col = -1;
-//                    mapRow++;
-//                    Row++;
-//                }
-//            }
-//        }
-//        // Stoping render of down wall
-//        else if ((mapCol > 0 && mapCol < gp.maxMapCol-1) && (mapRow > gp.maxMapRow)) {
-//            mapRow = gp.maxMapRow;
-//            while (Col <= gp.maxScreenCol + 1 && Row <= gp.maxScreenRow + 1) {
-//
-//                int worldX = mapCol * gp.TileSize;
-//                int worldY = mapRow * gp.TileSize;
-//                int screenX = worldX - gp.player.worldX + gp.player.screenX;
-//                int screenY = worldY - gp.player.worldY + gp.player.screenY;
-//
-//                if ((mapCol <= 0 || mapCol > gp.maxMapCol) || (mapRow <= 0 || mapRow > gp.maxMapRow)) {
-//                    currentMapNum = 0;
-//                } else {
-//                    currentMapNum = mapTileNum[mapRow][mapCol];
-//                }
-//                g2.drawImage(tile[currentMapNum].image, screenX, screenY, gp.TileSize, gp.TileSize, null);
-//                mapCol++;
-//                Col++;
-//
-//                if (Col == gp.maxScreenCol - 2) {
-//                    mapCol = (gp.player.worldX - gp.screenWidth / 2) / gp.TileSize;
-//                    Col = 0;
-//                    mapRow++;
-//                    Row++;
-//                }
-//            }
-//        }
-        // TODO: MapFromImg popraw tak aby wczytywalo tekstury i je obracalo.
     }
 }
--- src/help/ImageHelper.java
+++ src/help/ImageHelper.java
@@ -57,44 +57,71 @@ public class ImageHelper {
 
     public static boolean compareTwoImages(BufferedImage img1, BufferedImage img2)
     {
-        boolean outcomeFlag = true;
+        if (!checkImgSizes(img1, img2)) {
+            return false;
+        }
+
+        // Outer loop for rows(height)
+        for (int y = 0; y < img1.getHeight(); y++) {
+            // Inner loop for columns(width)
+            for (int x = 0; x < img1.getWidth(); x++) {
+                int rgbA = img1.getRGB(x, y);
+                int rgbB = img2.getRGB(x, y);
+                int redA = (rgbA >> 16) & 0xff;
+                int greenA = (rgbA >> 8) & 0xff;
+                int blueA = (rgbA) & 0xff;
+                int redB = (rgbB >> 16) & 0xff;
+                int greenB = (rgbB >> 8) & 0xff;
+                int blueB = (rgbB) & 0xff;
+
+                if (redA != redB || greenA != greenB || blueA != blueB) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Function which loops through all reflection of an img2 and checks if img1 is one of them
+    // if img1 is not one of them fun return -1 which will mean False in this case
+    // if img1 is one of them fun returns idx of reflection
+    // originalImg = 0; reflectedPerpendicularImg = 1; reflectedHorizontalImg = 2; bothReflectionsImg = 3;
+    public static int compareImgToReflections(BufferedImage img1, BufferedImage img2) {
+
+        if (!checkImgSizes(img1, img2)) {
+            return -1;
+        }
+
+        BufferedImage[] sides = {img2, imageReflectionPerpendicular(img2),
+                imageReflectionHorizontal(img2), imageReflectionPerpendicular(imageReflectionHorizontal(img2))};
+        BufferedImage tmpImg;
+        for (int idx=0; idx < sides.length; idx++) {
+            tmpImg = sides[idx];
+            if (compareTwoImages(img1, tmpImg)) {return idx;}
+        }
+
+        return -1;
+    }
+
+    // Function checks if two images are of the same size
+    public static boolean checkImgSizes(BufferedImage img1, BufferedImage img2) {
         int width1 = img1.getWidth();
         int width2 = img2.getWidth();
         int height1 = img1.getHeight();
         int height2 = img2.getHeight();
 
         // Checking whether the images are of same size or not
-        if ((width1 != width2) || (height1 != height2)) {
-            return false;
+        return (width1 == width2) && (height1 == height2);
+    }
+
+    public static BufferedImage reflectByType(BufferedImage img, int type) {
+        BufferedImage outImg = null;
+        switch (type) {
+            case 0 -> outImg = img;
+            case 1 -> outImg = imageReflectionPerpendicular(img);
+            case 2 -> outImg = imageReflectionHorizontal(img);
+            case 3 -> outImg = imageReflectionHorizontal(imageReflectionPerpendicular(img));
         }
-
-        BufferedImage[] sides = {img2, imageReflectionHorizontal(img2),
-                imageReflectionPerpendicular(img2), imageReflectionPerpendicular(imageReflectionHorizontal(img2))};
-
-        // Outer loop for rows(height)
-        for (BufferedImage tmpImg: sides) {
-            for (int y = 0; y < height1; y++) {
-                // Inner loop for columns(width)
-                for (int x = 0; x < width1; x++) {
-                    int rgbA = img1.getRGB(x, y);
-                    int rgbB = tmpImg.getRGB(x, y);
-                    int redA = (rgbA >> 16) & 0xff;
-                    int greenA = (rgbA >> 8) & 0xff;
-                    int blueA = (rgbA) & 0xff;
-                    int redB = (rgbB >> 16) & 0xff;
-                    int greenB = (rgbB >> 8) & 0xff;
-                    int blueB = (rgbB) & 0xff;
-
-                    if (redA != redB || greenA != greenB || blueA != blueB) {
-                        outcomeFlag = false;
-                    }
-                }
-            }
-            if (outcomeFlag) {
-                return true;
-            }
-            outcomeFlag = true;
-        }
-        return false;
+        return outImg;
     }
 }
